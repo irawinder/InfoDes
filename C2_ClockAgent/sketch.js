d9512b01946b9b11e30ee322789472bf0fac545b
irawinder <jamesirawinder@gmail.com>
2016-09-12 21:45:31 -0400

    Clock Agents for Seconds, Minutes, Hours

--- C2_ClockAgent/sketch.js
+++ C2_ClockAgent/sketch.js
@@ -2,68 +2,169 @@
 // Ira Winder, Sep 2016
 
 var col = "#999999";
-var hLength = 0.7;
-var mLength = 0.9;
-var sLength = 0.95;
-var sec = [];
-var numSec = 10;
+
+// Variables for holding integer time
+var h, m, s;
+
+// Arrays for holding agents for time visualization
+var seconds = [];
+var minutes = [];
+var hours = [];
+
+// Gravity Well Positions for Seconds, Minutes, and Hours
+var secX, secY, minX, minY, hrX, hrY;
 
 function setup() {
-  createCanvas(800, 800);
-  for (var i=0; i<numSec; i++) {
-    // Agent(diam, maxRepel, maxVel, seekAcc, repelAcc, posX, posY, targetX, targetY)
-    sec[i] = new Agent(30, 50, 10, 0.25, 1, random(width), random(height), 400, 400);
-  }
+  createCanvas(800, 400);
+  
+  // Gravity Well Positions for Seconds, Minutes, and Hours
+  secX = 3*width/4;
+  secY = height/2;
+  minX = width/2;
+  minY = height/2;
+  hrX = width/4;
+  hrY = height/2;
+  
+  // Initializes Clock Agents
+  initClock();
 }
 
 function draw() {
-  var h = hour();
-  var m = minute();
-  var s = second();
-  
+
   background(col);
   noStroke();
   
-  // var handScaler = 600;
+  // Updates Number of Agents
+  updateClockNum();
   
-  // var thetaH = map(h%12 + m/60.0, 0, 12, 0, TAU);
-  // var thetaM = map(m + s/60.0, 0, 60, 0, TAU);
-  // var thetaS = map(s, 0, 60, 0, TAU);
-  
-
-  
-  // fill(255);
-  // arc(width/2, height/2, handScaler*sLength, handScaler*sLength, -PI/2, thetaS-PI/2);
-  // fill(200);
-  // arc(width/2, height/2, handScaler*mLength, handScaler*mLength, -PI/2, thetaM-PI/2);
-  // fill(0);
-  // arc(width/2, height/2, handScaler*hLength, handScaler*hLength, -PI/2, thetaH-PI/2);
-  
-  for (var i=0; i<sec.length; i++) {
-    sec[i].applyBehavior();
-    ellipse(sec[i].posX, sec[i].posY, sec[i].diam, sec[i].diam);
-  }
+  // Updates Position of Agents
+  updateClockPos();
   
   text("frameRate: " + nf(frameRate(), 2, 1), 10, 20);
   
 }
 
-function Agent(diam, maxRepel, maxVel, seekAcc, repelAcc, posX, posY, targetX, targetY) {
+function initClock() {
+  var h = hour();
+  var m = minute();
+  var s = second();
+  
+  // Init Seconds
+  for (var i=0; i<s; i++) {
+    seconds[i] = newSecond();
+  }
+  
+  // Init Minutes
+  for (var i=0; i<m; i++) {
+    minutes[i] = new Agent(20, minX + random(-width/8, width/8), random(height), minX, minY);
+  }
+  
+  // Init Hours
+  for (var i=0; i<h; i++) {
+    hours[i] = new Agent(40, hrX + random(-width/8, width/8), random(height), hrX, hrY);
+  }
+}
+
+// Agent(diam, shade, maxVel, seekAcc, repelAcc, posX, posY, targetX, targetY)
+
+function newSecond() {
+  return new Agent(10, secX + random(-width/8, width/8), random(height), secX, secY);
+}
+
+function newMinute() {
+  return new Agent(20, secX, secY, minX, minY);
+}
+
+function newHour() {
+  return new Agent(40, minX, minY, hrX, hrY);
+}
+
+function updateClockNum() {
+  
+  // Update Agent Quantities
+  h = hour();
+  m = minute();
+  s = second();
+  
+  if (seconds.length < s) {
+    while(seconds.length < s) {
+      seconds.splice(seconds.length, 1, newSecond());
+    }
+  } else if (s == 0 && seconds.length > 0) {
+    seconds.splice(0, seconds.length);
+  }
+  
+  if (minutes.length < m) {
+    while(minutes.length < m) {
+      minutes.splice(minutes.length, 1, newMinute());
+    }
+  } else if (m == 0 && minutes.length > 0) {
+    minutes.splice(0, minutes.length);
+  }
+  
+  if (hours.length < h) {
+    while(hours.length < h) {
+      hours.splice(hours.length, 1, newHour());
+    }
+  } else if (h == 0 && hours.length > 0) {
+    hours.splice(0, hours.length);
+  }
+}
+
+function updateClockPos() {
+  
+  // Update Agent Positions
+  for (var i=0; i<seconds.length; i++) {
+    seconds[i].seekForce();
+    
+    for (var j=0; j<seconds.length; j++) 
+      if (i != j) 
+        seconds[i].repelForce(seconds[j].posX, seconds[j].posY);
+    
+    seconds[i].update();
+    seconds[i].display();
+  }
+  
+  for (var i=0; i<minutes.length; i++) {
+    minutes[i].seekForce();
+    
+    for (var j=0; j<minutes.length; j++) 
+      if (i != j) 
+        minutes[i].repelForce(minutes[j].posX, minutes[j].posY);
+    
+    minutes[i].update();
+    minutes[i].display();
+  }
+  
+  for (var i=0; i<hours.length; i++) {
+    hours[i].seekForce();
+    
+    for (var j=0; j<hours.length; j++) 
+      if (i != j) 
+        hours[i].repelForce(hours[j].posX, hours[j].posY);
+    
+    hours[i].update();
+    hours[i].display();
+  }
+  
+}
+
+function Agent(diam, posX, posY, targetX, targetY) {
   
   // Agent Diameter
   this.diam = diam;
   
-  // Max Repelling Distance
-  this.maxRepel = maxRepel;
+  // Color of Agent
+  this.shade = random(255);
   
   // Maximum Velocity [px/frame]
-  this.maxVel = maxVel;
+  this.maxVel = 0.5;
   
   // Maximum Seek Acceleration Modifier [px/frame^2]
-  this.seekAcc = seekAcc;
+  this.seekAcc = 0.5;
   
   // MAximum Repel Acceleration Modifier [px/frame^2]
-  this.repelAcc = repelAcc;
+  this.repelAcc = 0.5;
   
   // Agent Initial Postion
   this.posX = posX;
@@ -79,18 +180,9 @@ function Agent(diam, maxRepel, maxVel, seekAcc, repelAcc, posX, posY, targetX, t
   
   // Agent Acceleration
   this.accX = 0;
-  this.accY = 0.1;
+  this.accY = 0;
   
-  this.applyBehavior = function() {
-    
-    // Reset Accelartion Vector
-    this.accX = 0;
-    this.accY = 0;
-    
-    // Update Acceleration
-    this.seekForce();
-    this.repelForce(width/2, height/2);
-    
+  this.update = function() {
     // Update Velocity
     this.velX += this.accX;
     this.velY += this.accY;
@@ -106,6 +198,10 @@ function Agent(diam, maxRepel, maxVel, seekAcc, repelAcc, posX, posY, targetX, t
     // Update Position
     this.posX += this.velX;
     this.posY += this.velY;
+    
+    // Reset Acceleration Vector
+    this.accX = 0;
+    this.accY = 0;
   }
   
   this.seekForce = function() {
@@ -121,8 +217,8 @@ function Agent(diam, maxRepel, maxVel, seekAcc, repelAcc, posX, posY, targetX, t
     seekAccY /= magnitude;
     
     // Set Magnitude Acceleration Seek Vector
-    seekAccX *= seekAcc;
-    seekAccY *= seekAcc;
+    seekAccX *= this.seekAcc;
+    seekAccY *= this.seekAcc;
     
     // Add to Global Acceleration Vector
     this.accX += seekAccX;
@@ -137,7 +233,7 @@ function Agent(diam, maxRepel, maxVel, seekAcc, repelAcc, posX, posY, targetX, t
     
     // Apply rest of function if within repel radius [px]
     var distance = sqrt(sq(repelAccX) + sq(repelAccY));
-    if (distance < maxRepel) {
+    if (distance < diam) {
     
       // Normalize Acceleration Repel Vector
       var magnitude = distance;
@@ -145,12 +241,17 @@ function Agent(diam, maxRepel, maxVel, seekAcc, repelAcc, posX, posY, targetX, t
       repelAccY /= magnitude;
       
       // Set Magnitude Acceleration Seek Vector
-      repelAccX *= repelAcc;
-      repelAccY *= repelAcc;
+      repelAccX *= this.repelAcc;
+      repelAccY *= this.repelAcc;
       
       // Add to Global Acceleration Vector
       this.accX += repelAccX;
       this.accY += repelAccY;
     }
+  }
+  
+  this.display = function() {
+    fill(this.shade);
+    ellipse(this.posX, this.posY, this.diam, this.diam);
   }
 }
